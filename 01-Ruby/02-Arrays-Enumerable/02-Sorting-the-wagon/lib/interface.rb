cb8f352ad841c9e01a0a9b64010dde3dc58a0bb7
judepim <judepim@gmail.com>
2014-10-08 16:16:31 +0200

    exo 2

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -5,7 +5,23 @@ require_relative "wagon_sort"
 #       - `gets`:  http://www.ruby-doc.org/core-2.1.2/Kernel.html#method-i-gets
 #       - `chomp`: http://www.ruby-doc.org/core-2.1.2/String.html#method-i-chomp
 
+students = []
+name = "0"
 
+while name.length != 0
+  puts "type a student name"
+  name = gets.chomp
+  if name != ""
+    students << name
+  end
+end
 
-# TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
-#       file and display the sorted student list
+student_number = wagon_sort(students)
+
+puts "Congratulations! Your Wagon has #{student_number.size} students:"
+if student_number.size >= 2
+  puts "#{student_number[0..-2].join(',')} and #{student_number.last}"
+else
+  puts student_number.first
+end
+
