4ba483da228079c36014fd5582adae1b61c2dd29
judepim <judepim@gmail.com>
2014-10-08 12:11:59 +0200

    exo ok

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,16 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  while your_message != "I am going to work right now"
+    puts "coach_answer"
+    until your_message == "I am going to work right now"
+      puts "coach_answer_enhanced"
 end
+
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
+  return  your_message
+  unless your_message == "I am going to work right now"
+    print "coach_answer_enhanced"
 end
 
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,7 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+
+puts "your_message"
+your_message = gets.chomp
+print "coach_answer"
+
