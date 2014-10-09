a7cbb8d27a63984fda9ef55506b613a5fa86756a
judepim <judepim@gmail.com>
2014-10-09 09:00:06 +0200

    black jack

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
@@ -1,10 +1,8 @@
 def pick_bank_score
-  # TODO: Use Random to get a new random score
-  rand(16..21)
+  Random.rand(16..21)
 end
 
 def pick_player_card
-  # TODO: Use Random to get a new random card
-  rand(1..11)
+  Random.rand(1..11)
 end
 
--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -1,35 +1,42 @@
 require_relative 'black_jack'
 
-def state_of_the_game(player_score, bank_score)
-  # TODO: return (not print!) a message containing the player's score and bank's score
-  return "your score is #{player_score}, the bank is #{bank_score}"
+def state_of_the_game(score, bank)
+  "Your score is #{score}, bank is #{bank}!"
 end
 
 def end_game_message(player_score, bank_score)
-  # TODO: return (not print!) a message telling if the user won or lost.
   if player_score > 21
-      return "you lost"
-    elsif player_score == 21
-      return "black jack"
-    elsif player_score > bank_score
-      return "win"
-    elsif player_score < bank_score
-      return "you lost"
+    "You are over 21... you lose."
+  elsif player_score == 21
+    "Black Jack!"
+  elsif player_score > bank_score
+    "You beat the bank! You win."
+  else
+    "Bank beats you! You lose."
   end
 end
 
+def asking_for_card?(score)
+  # If the score is more than 21 we immediately exit the method by returning false
+  return false if score > 21
+
+  puts "Card ? (Type 'y' or 'yes' for a new card)"
+  print "> "
+  gets.chomp.match(/y/i)
+end
 
 def play_game
-  # TODO: make the user play from terminal in a while loop that will stop
-  #       when the user will not be asking for  a new card
-  score_player = pick_player_card
-  score_bank = pick_bank_score
-  answer = "yes"
-  while (answer == "y" || answer == "yes") && (score_player <= 21)
-    puts state_of_the_game(player_score, bank_score)
-    puts "Do you want a card? (y or yes otherwise no card)"
-    answer = gets.chomp
-  end
-  puts end_game_message (score_player,score_bank)
+  puts "*** WELCOME TO OUR FANCY BLACKJACK ***"
 
+  bank_score = pick_bank_score
+  player_score = 0
+
+  # While we are the user asks for a card (and has a score < 21)
+  while asking_for_card?(player_score)
+    player_score += pick_player_card
+    puts state_of_the_game(player_score, bank_score)
+  end
+
+  puts end_game_message(player_score, bank_score)
 end
+
