fd535b98e0c28d9cebfef5078856e0cb306b64e6
judepim <judepim@gmail.com>
2014-10-23 16:03:20 +0200

    ok

--- 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
@@ -2,16 +2,21 @@ require 'sqlite3'
 
 def number_of_rows(db, table_name)
   # TODO: count number of rows in table table_name
+  db.execute("SELECT COUNT(*) FROM #{table_name}").flatten[0]
 end
 
 def sorted_artists(db)
   # TODO: return array of artists' names sorted alphabetically
+  # order by
+  db.execute("SELECT name FROM artists ORDER BY name ASC").flatten
 end
 
 def love_tracks(db)
-  # TODO: return array of love songs
+  # TODO: return array of love songs - use where and like
+  db.execute("SELECT name FROM tracks WHERE name LIKE '%love%'").flatten
 end
 
 def long_tracks(db, min_length)
-  # TODO: return tracks verifying: duration > min_length (minutes)
+  # TODO: return tracks verifying: duration > min_length (minutes) - use >
+  db.execute("SELECT name FROM tracks WHERE (milliseconds / 60000) >=#{min_length}").flatten
 end
