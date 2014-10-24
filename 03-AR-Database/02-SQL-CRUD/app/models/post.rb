70a0e695834aeecf178cc3a8cb80eac510774649
judepim <judepim@gmail.com>
2014-10-24 15:47:13 +0200

    ok

--- 03-AR-Database/02-SQL-CRUD/app/models/post.rb
+++ 03-AR-Database/02-SQL-CRUD/app/models/post.rb
@@ -3,11 +3,60 @@
 
 class Post
   # TODO: implement some reader and/or writers
+  attr_reader :id, :date
+  attr_accessor :votes, :title, :url
 
   def initialize(options = {})
     # TODO: initialize instance variables from the hash options.
+    @id = options[:id]
+    @title = options[:title]
+    @date = options[:date]
+    @url = options[:url]
+    @votes = options[:votes]
+
+    if options[:date] == nil
+      @date = Time.now
+    end
+
+    if options[:votes] == nil
+      @votes = 0
+    end
   end
 
-  # TODO: implement all the class methods and instance methods
-  #       to have a full CRUD support on your Post model
+  def upvote
+    @votes = @votes +1
+  end
+
+  def save
+    DB.execute("INSERT INTO posts(title, url, date, votes)
+      VALUES('#{@title}','#{@url}','#{@date}','#{votes}');")
+    @id = DB.last_insert_row_id
+      if @id == nil
+        DB.excute("INSERT INTO post (title, votes, url)")
+      else
+        DB.execute("UPDATE post SET title ='#{title}', url='#{url}', date = '#{date}', vote = '#{vote}'")
+  end
+
+  def destroy(post)
+    DB.execute("DELETE FROM posts WHERE  id = #{@id}")
+  end
+
+  def self.find (id_numb)
+    columns = DB.execute("SELECT * FROM posts WHERE id = #{id_numb};").first
+    if columns == nil
+     post = nil
+      else
+    post = Post.new({ id: columns[0], title: columns[1], url: columns[2], date: Time.at(columns[3]), votes: columns[4] })
+    end
+  end
+
+  def self.all
+    rows = DB.execute("SELECT * FROM posts ;")
+    posts = []
+    rows.each do |columns|
+    posts << Post.new({ id: columns[0], title: columns[1], url: columns[2], date: Time.at(columns[3]), votes: columns[4] })
+   end
+   return posts
+  end
+
 end
